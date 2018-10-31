3e16be6f5569a5f82c211e31c98aedea538ddea9
NeoTheSecond <quangman1404@gmail.com>
2018-10-31 14:26:26 +0700

    some changes

--- client/src/components/product/customizeOptionsList.js
+++ client/src/components/product/customizeOptionsList.js
@@ -11,11 +11,14 @@ export default class CustomizeOptionsList extends React.Component {
         }
         this.handlePick = this.handlePick.bind(this)
     }
+
+
+
     handlePick(event){
         event.preventDefault();
         //name chính là category
         let selectedCustomizeOption = this.state.selectedCustomizeOptions
-        if(this.state.selectedCustomizeOptions.length !== 0){
+        if(selectedCustomizeOption.length !== 0){
             if (selectedCustomizeOption.filter(customizeOption => customizeOption.category === event.target.name ).length > 0){
                 selectedCustomizeOption.forEach(customizeOption => {
                     if(customizeOption.category === event.target.name){
@@ -31,28 +34,41 @@ export default class CustomizeOptionsList extends React.Component {
                 })
             }
         }else{
-            let selectedCustomizeOption = []
             selectedCustomizeOption.push({
                 category: event.target.name,
                 option: event.target.value,
                 img: event.target.id
             })
         }
-        this.setState((prevState) => ({
-            selectedCustomizeOptions: [...prevState.selectedCustomizeOptions, selectedCustomizeOption]
-        }))
+        this.setState({
+            selectedCustomizeOptions: selectedCustomizeOption
+        })
     }
+
+
+    handleAddToCart = e => {
+        this.state.selectedCustomizeOptions.forEach((selectedCustomizeOption) => {
+            console.log(selectedCustomizeOption)
+        })
+    }
+
+
+
     render(){
+        // RENDER OPTION
         const optionsList = (options,category) => {
             return options.map((option, index) => <Input key={index} type="button" value={option.name} name={category} onClick={this.handlePick} id={option.img}/>)
         }
 
+        // RENDER CUSTOMIZE OPTION LIST
         const customizeOptionList = this.props.customizeOption.map((customizeOption, index) =>
             <ListGroup key={index}>
                 <ListGroupItem><Input type="button" value={customizeOption.category}/></ListGroupItem>
                 {optionsList(customizeOption.options, customizeOption.category)}
             </ListGroup>
         );
+
+        // IMAGES FOR SELECTED CUSTOMIZE OPTIONS
         let selectedCustomizeOptions = this.state.selectedCustomizeOptions.map((customizeOption, index) =>
             <img key={index} src={customizeOption.img} className="image"/>
         )
@@ -63,6 +79,9 @@ export default class CustomizeOptionsList extends React.Component {
                     <Container className="container">
                         <img src={this.props.baseProduct} className="image"></img>
                         {selectedCustomizeOptions}
+                    </Container>
+                    <Container>
+                        <Button color="primary" onClick={this.handleAddToCart}>ADD TO CART</Button>
                     </Container>
                 </Container>
         )
